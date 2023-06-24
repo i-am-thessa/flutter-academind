94177cc5da4967e80abb1a9c08c577c8de394e88
iamthessa <theresa.artillero.dev@gmail.com>
2023-06-24 08:45:27 +0800

    Updated quiz_app

--- quiz_app/lib/start_quiz.dart
+++ quiz_app/lib/start_quiz.dart
@@ -1,43 +1,27 @@
-import 'dart:math';
-import 'package:flutter/material.dart';
-import 'package:quiz_app/styled_text.dart';
+// import 'dart:math';
+// import 'package:flutter/material.dart';
+// import 'package:quiz_app/styled_text.dart';
 
-final randomizer = Random();
+// final randomizer = Random();
 
-class StartQuiz extends StatefulWidget {
-  const StartQuiz({super.key});
+// class StartQuiz extends StatefulWidget {
+//   const StartQuiz({super.key});
 
-  @override
-  State<StartQuiz> createState() {
-    return _StartQuizState();
-  }
-}
+//   @override
+//   State<StartQuiz> createState() {
+//     return _StartQuizState();
+//   }
+// }
 
-class _StartQuizState extends State<StartQuiz> {
-  void startQuiz() {
-    setState(() {
-      // Todo do something
-    });
-  }
+// class _StartQuizState extends State<StartQuiz> {
+//   void startQuiz() {
+//     setState(() {
+//       // Todo do something
+//     });
+//   }
 
-  @override
-  Widget build(context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset('assets/images/quiz-logo.png', width: 200),
-        const SizedBox(
-          height: 50,
-        ),
-        const StyledText('Learn Flutter the fun way!'),
-        const SizedBox(
-          height: 50,
-        ),
-        FilledButton(
-          onPressed: startQuiz,
-          child: const Text('Start Quiz'),
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(context) {
+//     return const Text('data');
+//   }
+// }
--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_quiz.dart';
-
-const startAlignment = Alignment.topLeft;
-const endAlignment = Alignment.bottomRight;
 
 class StartScreen extends StatelessWidget {
-  const StartScreen(this.color1, this.color2, {super.key});
 
-  const StartScreen.purple({super.key})
-      : color1 = const Color.fromARGB(255, 83, 12, 207),
-        color2 = Colors.indigo;
-
-  final Color color1;
-  final Color color2;
+  const StartScreen({super.key});
 
   @override
   Widget build(context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color1, color2],
-          begin: startAlignment,
-          end: endAlignment,
-        ),
-      ),
-      child: const Center(
-        child: StartQuiz(),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png', 
+            width: 300,
+            color: const Color.fromARGB(180, 255, 255, 255),
+          ),
+          // Opacity( --> bad practice
+          //   opacity: 0.5,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png', 
+          //     width: 300
+          //   ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+             color: Color.fromARGB(255, 237, 223, 252),
+             fontSize: 24
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
       ),
     );
   }
