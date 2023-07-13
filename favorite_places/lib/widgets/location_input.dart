84a9223ad512ee40a48100378b7f2a6d4655c594
iamthessa <theresa.artillero.dev@gmail.com>
2023-07-13 18:23:14 +0800

    Added setting of location

--- favorite_places/lib/widgets/location_input.dart
+++ favorite_places/lib/widgets/location_input.dart
@@ -1,16 +1,31 @@
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LocationInput extends StatefulWidget {
-  const LocationInput({super.key});
+  const LocationInput({super.key, required this.onSelectLocation});
+
+  final void Function(PlaceLocation location) onSelectLocation;
 
   @override
   State<LocationInput> createState() => _LocationInputState();
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
+
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:ref%7Clabel:A%7C$lat,$lng&key=YOURAPIKEY';
+  }
 
   void _getCurrentLocation() async {
     Location location = Location();
@@ -21,43 +36,69 @@ class _LocationInputState extends State<LocationInput> {
 
     serviceEnabled = await location.serviceEnabled();
 
-    if(!serviceEnabled) {
+    if (!serviceEnabled) {
       serviceEnabled = await location.requestService();
-      if(!serviceEnabled) {
+      if (!serviceEnabled) {
         return;
       }
     }
 
     permissionGranted = await location.hasPermission();
-    if(permissionGranted == PermissionStatus.denied) {
+    if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
-      if(permissionGranted != PermissionStatus.granted) {
+      if (permissionGranted != PermissionStatus.granted) {
         return;
       }
     }
-   
-   setState(() {
-     _isGettingLocation = true;
-   });
 
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
 
-   locationData = await location.getLocation();
+    if (lat == null || lng == null) {
+      return;
+    }
 
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=YOUR_API_KEY'); //TODO
 
+    final response = await http.get(url);
+    final resData = json.decode(response.body);
+    final address = resData['results'][0]['formatted_address'];
+
+    setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: lat,
+        longitude: lng,
+        address: address,
+      );
+      _isGettingLocation = true;
+    });
+
+    widget.onSelectLocation(_pickedLocation!);
   }
 
   @override
   Widget build(BuildContext context) {
     Widget previewContent = Text(
-            'No location chosen',
-            textAlign: TextAlign.center,
-            style: Theme.of(context)
-                .textTheme
-                .bodyLarge!
-                .copyWith(color: Theme.of(context).colorScheme.onBackground),
-          );
+      'No location chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context)
+          .textTheme
+          .bodyLarge!
+          .copyWith(color: Theme.of(context).colorScheme.onBackground),
+    );
 
-    if(_isGettingLocation) {
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
+
+    if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
     }
 
